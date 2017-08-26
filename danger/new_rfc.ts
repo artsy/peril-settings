97bfd625aaab99c68331d013c350e79c084d0f38
Orta Therox <orta.therox@gmail.com>
2017-08-25 21:49:13 -0400

    USe the real url, and link to danger issues

--- danger/new_rfc.ts
+++ danger/new_rfc.ts
@@ -1,7 +1,7 @@
 import { schedule, danger } from "danger";
 import { IncomingWebhook } from "@slack/client";
 
-declare const peril: any
+declare const peril: any // danger/danger#351
 
 const gh = danger.github as any // danger/peril#128
 const issue = gh.issue
@@ -16,7 +16,7 @@ if (issue.title.includes("RFC:")) {
         pretext: "🎉 A new Peril RFC has been published.",
         color: "good",
         title: issue.title,
-        title_link: issue.url,
+        title_link: issue.html_url,
         author_name: issue.user.login,
         author_icon: issue.user.avatar_url
       }]
