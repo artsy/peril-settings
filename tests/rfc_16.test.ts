015db53fc0601a506d045d4e8b75747a6197944f
Orta Therox <orta.therox@gmail.com>
2017-09-28 10:45:23 -0400

    Simplify rfc 16 a bit

--- tests/rfc_16.test.ts
+++ tests/rfc_16.test.ts
@@ -1,6 +1,6 @@
 jest.mock("danger", () => jest.fn())
 import * as danger from "danger"
-const dm = (danger as any)
+const dm = danger as any
 
 import { rfc16 } from "../org/all-prs"
 
@@ -9,19 +9,29 @@ beforeEach(() => {
   dm.warn = jest.fn()
 })
 
-afterEach(() => {
-  dm.warn = undefined
-})
+const pr = {
+  head: {
+    user: {
+      login: "danger",
+    },
+    repo: {
+      name: "danger-js",
+    },
+  },
+}
 
 it("warns when code has changed but no changelog entry was made", () => {
   dm.danger.github = {
-    utils: {
-      fileContents: () => Promise.resolve("some changes")
-    }
+    api: {
+      repos: {
+        getContent: () => Promise.resolve([{ name: "code.js" }, { name: "CHANGELOG.md" }]),
+      },
+    },
+    pr,
   }
   dm.danger.git = {
     modified_files: ["src/index.html"],
-    created_files: []
+    created_files: [],
   }
   return rfc16().then(() => {
     expect(dm.warn).toBeCalled()
@@ -30,13 +40,16 @@ it("warns when code has changed but no changelog entry was made", () => {
 
 it("does nothing when there is no changelog file", () => {
   dm.danger.github = {
-    utils: {
-      fileContents: () => Promise.resolve(null)
-    }
+    api: {
+      repos: {
+        getContent: () => Promise.resolve([{ name: "code.js" }]),
+      },
+    },
+    pr,
   }
   dm.danger.git = {
     modified_files: [],
-    created_files: []
+    created_files: [],
   }
   return rfc16().then(() => {
     expect(dm.warn).not.toBeCalled()
@@ -45,13 +58,16 @@ it("does nothing when there is no changelog file", () => {
 
 it("does nothing when only `test` files were changed", () => {
   dm.danger.github = {
-    utils: {
-      fileContents: () => Promise.resolve("some changes")
-    }
+    api: {
+      repos: {
+        getContent: () => Promise.resolve([{ name: "CHANGELOG.md" }]),
+      },
+    },
+    pr,
   }
   dm.danger.git = {
     modified_files: ["tests/AuctionCalculatorSpec.scala"],
-    created_files: []
+    created_files: [],
   }
   return rfc16().then(() => {
     expect(dm.warn).not.toBeCalled()
@@ -60,13 +76,16 @@ it("does nothing when only `test` files were changed", () => {
 
 it("does nothing when the changelog was changed", () => {
   dm.danger.github = {
-    utils: {
-      fileContents: () => Promise.resolve("some changes")
-    }
+    api: {
+      repos: {
+        getContent: () => Promise.resolve([{ name: "code.js" }, { name: "CHANGELOG.md" }]),
+      },
+    },
+    pr,
   }
   dm.danger.git = {
     modified_files: ["src/index.html", "CHANGELOG.md"],
-    created_files: []
+    created_files: [],
   }
   return rfc16().then(() => {
     expect(dm.warn).not.toBeCalled()
