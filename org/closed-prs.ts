f805dab477e6edf3cb2ae029311ea62cf553ab86
Orta Therox <orta.therox@gmail.com>
2017-12-01 15:15:44 -0500

    Improve the wording in RFC33

--- org/closed-prs.ts
+++ org/closed-prs.ts
@@ -44,9 +44,12 @@ export const rfc33 = rfc("Ping slack channels for related labels", async () => {
       attachments: [
         {
           color: "good",
-          title: `Merged: <${pr.base.repo.html_url}/pull/${pr.number}|${pr.title}> from ${pr.user.login}`,
+          title: `Merged PR on <${pr.base.repo.name}|${pr.base.repo.html_url}>: <${pr.base.repo.html_url}/pull/${
+            pr.number
+          }|${pr.title}>`,
           title_link: pr.base.repo.html_url,
           author_name: pr.user.login,
+          author_icon: (pr.user as any).avatar_url, // This'll be fixed in the next Danger 2.x release
         },
       ],
     })
