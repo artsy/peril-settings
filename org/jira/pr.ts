a4cc5d799dc897d6ecb31b00c56bfa3a5c970ed3
Orta Therox <orta.therox@gmail.com>
2018-11-18 08:02:25 -0500

    Fix logs

--- org/jira/pr.ts
+++ org/jira/pr.ts
@@ -43,7 +43,7 @@ export default async (webhook: PullRequest) => {
       // https://developer.atlassian.com/cloud/jira/platform/rest/#api-api-2-issue-issueIdOrKey-get
 
       const issue = await jira.findIssue(ticketID)
-      console.log(`Status: ${issue.fields.status.name}`)
+      console.log(`Status: ${issue.fields.status}`)
       console.log(`Description: ${issue.fields.description}`)
       console.log(`url: ${issue.self}`)
 
