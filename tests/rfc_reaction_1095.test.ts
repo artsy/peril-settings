af4441385ae466be0475fbdd58f50153d8315f50
Christopher Pappas <damassi.pappas@gmail.com>
2019-01-04 12:01:19 -0800

    Make addPatchLabel global

--- tests/rfc_reaction_1095.test.ts
+++ tests/rfc_reaction_1095.test.ts
@@ -1,6 +1,9 @@
 jest.mock("danger", () => ({
   danger: {
     github: {
+      utils: {
+        fileContents: jest.fn(),
+      },
       pr: {
         number: 12,
         base: {
@@ -31,17 +34,28 @@ import { danger } from "danger"
 const mockGetLabels: jest.Mock = danger.github.api.issues.getLabels as any
 const mockCreateLabel: jest.Mock = danger.github.api.issues.createLabel as any
 const mockAddLabels: jest.Mock = danger.github.api.issues.addLabels as any
+const mockfileContents: jest.Mock = danger.github.utils.fileContents as any
 
-import addPatchLabel, { labels } from "../repos/reaction/addPatchLabel"
+import addPatchLabel, { labels } from "../org/addPatchLabel"
 
 afterEach(() => {
   mockGetLabels.mockReset()
   mockCreateLabel.mockReset()
   mockAddLabels.mockReset()
+  mockfileContents.mockReset()
+})
+
+it("Does nothing if there is no autorc", async () => {
+  mockfileContents.mockResolvedValueOnce("")
+
+  await addPatchLabel()
+
+  expect(danger.github.api.issues.getLabels).not.toBeCalled()
 })
 
 it("Does nothing if there's already a release label", async () => {
   danger.github.issue.labels = [{ name: "Version: Major" } as any]
+  mockfileContents.mockResolvedValueOnce("{}")
 
   await addPatchLabel()
 
@@ -53,6 +67,7 @@ it("Creates labels for this repo if there are no labels yet", async () => {
   danger.github.issue.labels = []
   // nothing set up in the repo yet
   mockGetLabels.mockResolvedValueOnce({ data: [] })
+  mockfileContents.mockResolvedValueOnce("{}")
 
   await addPatchLabel()
 
@@ -67,6 +82,7 @@ it("Posts a patch label if there are no labels already added", async () => {
   danger.github.issue.labels = []
   // the repo already has labels set up
   mockGetLabels.mockResolvedValueOnce({ data: [{ name: "Version: Patch" } as any] })
+  mockfileContents.mockResolvedValueOnce("{}")
 
   await addPatchLabel()
 
