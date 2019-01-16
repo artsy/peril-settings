fea4fe5248c67949eddaf154654d58661c92e36d
Isac Petruzzi <isac.petruzzi@gmail.com>
2019-01-16 18:52:16 +0100

    resolve metaphysics production SHA1 tag and get schema

--- upstream/metaphysics.ts
+++ upstream/metaphysics.ts
@@ -1,7 +1,37 @@
 import { danger, warn } from "danger"
-import AWS from "aws-sdk"
+import { ECR } from "aws-sdk"
+
+const resolveSHA1ForTag = (ecr: ECR, tag: string) => {
+  return new Promise((resolve, reject) => {
+    const re = new RegExp("^[0-9a-f]{40}$")
+    ecr.describeImages({repositoryName: 'metaphysics',
+                        imageIds: [{imageTag: tag}]},
+    (err, data) => {
+        if (err) {
+            reject(err)
+        } else {
+          if (data.imageDetails.length != 1) {
+            reject(new Error(`Found multiple images matching tag "${tag}"`))
+          }
+          data.imageDetails[0].imageTags.forEach((imageTag) => {
+              if (re.test(imageTag)) {
+                  resolve(imageTag)
+              }
+          })
+          reject(new Error(`Cound not find a Git-SHA1 tag for image "${data.imageDetails[0].imageDigest}"`))
+        }
+    })
+  })
+}
 
 export default async () => {
-  const ecr = new AWS.ECR()
-  // Grab tags, compare them
+  const ecr = new ECR()
+  let productionSHA1
+  try {
+    productionSHA1 = await resolveSHA1ForTag(ecr, 'production')
+    return await danger.github.utils.fileContents("_schema.graphql", `artsy/metaphysics`, productionSHA1)
+  } catch(err) {
+    console.log(err)
+    return null
+  }
 }
