cf2d64a510eb9b75fbb70d908076f936cb3d7ee1
Justin Bennett <zephraph@gmail.com>
2019-01-24 12:31:15 -0500

    Change mog commit to be title of PR

--- org/mergeOnGreen.ts
+++ org/mergeOnGreen.ts
@@ -35,8 +35,11 @@ export const rfc10 = async (status: Status) => {
       return console.log("PR does not have Merge on Green")
     }
 
+    // Strip any "@user =>" prefixes from the pr title
+    const prTitle = issue.data.title.replace(/@(\w|-)+\s+=>\s+/, "")
+
     // Merge the PR
-    await api.pullRequests.merge({ owner, repo, number, commit_title: `Merge pull request #${number} by Peril` })
+    await api.pullRequests.merge({ owner, repo, number, commit_title: `${prTitle} (#${number})` })
     console.log(`Merged Pull Request ${number}`)
   }
 }
