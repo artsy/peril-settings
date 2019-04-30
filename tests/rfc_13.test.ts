bded8bc83b75d7f9f6dc38351d5eb428f7010fed
Zephraph <zephraph@gmail.com>
2019-04-30 11:39:09 -0400

    Add test to ensure doesn't warn no assignee on renovate pr

--- tests/rfc_13.test.ts
+++ tests/rfc_13.test.ts
@@ -9,7 +9,7 @@ beforeEach(() => {
   dm.warn = jest.fn()
 })
 
-it("warns when there's  no assignee and no WIP in the title", async () => {
+it("warns when there's no assignee and no WIP in the title", async () => {
   // When the check membership API call does not raise
   const api = { orgs: { checkMembership: () => {} } }
   dm.danger.github = { pr: { title: "My Thing", assignee: null, user: { login: "someone" } }, api }
@@ -19,7 +19,7 @@ it("warns when there's  no assignee and no WIP in the title", async () => {
   )
 })
 
-it("does not warns when someone does nto have access to the org", async () => {
+it("does not warns when someone does not have access to the org", async () => {
   // When the check membership API call does not raise
   const api = {
     orgs: {
@@ -34,13 +34,19 @@ it("does not warns when someone does nto have access to the org", async () => {
 })
 
 it("does not warn when there's there's no assignee and WIP in the title", async () => {
-  dm.danger.github = { pr: { title: "[WIP] My thing", assignee: null } }
+  dm.danger.github = { pr: { title: "[WIP] My thing", assignee: null, user: { login: "someone" } } }
   await rfc13()
   expect(dm.warn).not.toBeCalled()
 })
 
 it("does not warn when there's there's an assignee", async () => {
-  dm.danger.github = { pr: { title: "My thing", assignee: {} } }
+  dm.danger.github = { pr: { title: "My thing", assignee: {}, user: { login: "someone" } } }
+  await rfc13()
+  expect(dm.warn).not.toBeCalled()
+})
+
+it("does not warn when the PR is created by renovate", async () => {
+  dm.danger.github = { pr: { title: "My thing", assignee: null, user: { login: "renovate" } } }
   await rfc13()
   expect(dm.warn).not.toBeCalled()
 })
