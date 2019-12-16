a6b78cb58ede49463c0510dfed05b0f07a97357c
Ash Furrow <ash@ashfurrow.com>
2019-12-16 14:45:58 -0500

    Updates Jira ticket regular expression.
    Co-authored-by: Devon Blandin <dblandin@gmail.com>

--- tests/rfc_74.test.ts
+++ tests/rfc_74.test.ts
@@ -4,31 +4,41 @@ const dm = danger as any
 import { getJiraTicketIDsFromText, getJiraTicketIDsFromCommits } from "../org/jira/utils"
 
 describe("grabbing links", () => {
-  it("handles small references aka ABC-123", () => {
-    const body = "ok ABC-123 sure"
+  it("handles small references with parens aka (ABC-123)", () => {
+    const body = "ok (ABC-123) sure"
     expect(getJiraTicketIDsFromText(body)).toEqual(["ABC-123"])
   })
 
-  it("handles url references", () => {
-    const body = "ok https://artsyproduct.atlassian.net/browse/PLATFORM-46 sure"
-    expect(getJiraTicketIDsFromText(body)).toEqual(["PLATFORM-46"])
+  it("handles small references aka [ABC-123]", () => {
+    const body = "ok [ABC-123] sure"
+    expect(getJiraTicketIDsFromText(body)).toEqual(["ABC-123"])
   })
 
-  it("handles both url references and shorthand", () => {
-    const body = "ok I fixed DAN-145 and https://artsyproduct.atlassian.net/browse/PLATFORM-46 sure"
-    expect(getJiraTicketIDsFromText(body)).toEqual(["DAN-145", "PLATFORM-46"])
+  it("handles multiple small references aka [ABC-123] [DEF-456]", () => {
+    const body = "ok [ABC-123] [DEF-456] sure"
+    expect(getJiraTicketIDsFromText(body)).toEqual(["ABC-123", "DEF-456"])
+  })
+
+  it("ignores url references", () => {
+    const body = "ok https://artsyproduct.atlassian.net/browse/PLATFORM-46 sure"
+    expect(getJiraTicketIDsFromText(body)).toEqual([])
+  })
+
+  it("ignores url references but finds shorthand ones", () => {
+    const body = "ok I fixed [DAN-145] and opened https://artsyproduct.atlassian.net/browse/PLATFORM-46 for follow-up"
+    expect(getJiraTicketIDsFromText(body)).toEqual(["DAN-145"])
   })
 
   it("gets them out of the commits in danger", () => {
     const commits = [
       {
-        message: "OK PLAT-123 fixed",
+        message: "OK [PLAT-123] fixed",
       },
       {
         message: "bah, broke",
       },
       {
-        message: "Also did PLAT-124",
+        message: "Also did (PLAT-124)",
       },
       {
         message: "[DAN-32] Got some stuff",
